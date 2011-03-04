338d2d0f47274977bf51873803b6168cc7cc3171
John-Michael Glenn <phyre19@gmail.com>
2011-03-03 19:25:29 -0800

    Fixed code to work with newer builds and implemented the "Too many open files" fix

--- src/com/sparkedia/valrix/ColorMe/Property.java
+++ src/com/sparkedia/valrix/ColorMe/Property.java
@@ -14,10 +14,12 @@ import java.util.logging.Logger;
 
 public final class Property {
 	private static final Logger log = Logger.getLogger("Minecraft");
+	protected ColorMe plugin;
 	private Properties properties;
 	private String fileName;
 
-	public Property(String fileName) {
+	public Property(String fileName, ColorMe plugin) {
+		this.plugin = plugin;
 		this.fileName = fileName;
 		this.properties = new Properties();
 		File file = new File(fileName);
@@ -31,17 +33,21 @@ public final class Property {
 
 	public void load() {
 		try {
-			this.properties.load(new FileInputStream(this.fileName));
+			FileInputStream inFile = new FileInputStream(this.fileName);
+			this.properties.load(inFile);
+			inFile.close();
 		} catch (IOException ex) {
-			log.log(Level.SEVERE, "[ColorMe]: Unable to load "+this.fileName, ex);
+			log.log(Level.SEVERE, "["+plugin.pName+"]: Unable to load "+this.fileName, ex);
 		}
 	}
 
 	public void save() {
 		try {
-			this.properties.store(new FileOutputStream(this.fileName), "Minecraft Properties File");
+			FileOutputStream outFile = new FileOutputStream(this.fileName);
+			this.properties.store(outFile, "Minecraft Properties File");
+			outFile.close();
 		} catch (IOException ex) {
-			log.log(Level.SEVERE, "[ColorMe]: Unable to save "+this.fileName, ex);
+			log.log(Level.SEVERE, "["+plugin.pName+"]: Unable to save "+this.fileName, ex);
 		}
 	}
 
