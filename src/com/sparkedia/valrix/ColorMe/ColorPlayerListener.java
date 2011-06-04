a4e148acfa2df8a8e25b3988cbf6ffa05967f6cd
James Robinson <robinjam304@hotmail.com>
2011-06-04 03:33:58 +0100

    Set player's colour on join

--- src/com/sparkedia/valrix/ColorMe/ColorPlayerListener.java
+++ src/com/sparkedia/valrix/ColorMe/ColorPlayerListener.java
@@ -2,7 +2,7 @@ package com.sparkedia.valrix.ColorMe;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 
 public class ColorPlayerListener extends PlayerListener {
@@ -13,20 +13,20 @@ public class ColorPlayerListener extends PlayerListener {
 		this.plugin = plugin;
 		this.colors = plugin.colors;
 	}
-	
-	@Override
-	public void onPlayerChat(PlayerChatEvent event) {
-		Player player = event.getPlayer();
-		String name = player.getName().toLowerCase();
-		if (colors.keyExists(name) && plugin.hasColor(name)) {
-			String color = colors.getString(name);
-			for (int i = 0; i <= 15; i++) {
-				String col = ChatColor.getByCode(i).name();
-				if (color.equalsIgnoreCase(col.toLowerCase().replace("_", ""))) {
-					player.setDisplayName(ChatColor.valueOf(col)+ChatColor.stripColor(player.getName())+ChatColor.WHITE);
-					break;
-				}
-			}
-		}
-	}
+
+        @Override
+        public void onPlayerJoin(PlayerJoinEvent event) {
+            Player player = event.getPlayer();
+            String name = player.getName().toLowerCase();
+            if (colors.keyExists(name) && plugin.hasColor(name)) {
+                    String color = colors.getString(name);
+                    for (int i = 0; i <= 15; i++) {
+                            String col = ChatColor.getByCode(i).name();
+                            if (color.equalsIgnoreCase(col.toLowerCase().replace("_", ""))) {
+                                    player.setDisplayName(ChatColor.valueOf(col)+ChatColor.stripColor(player.getName())+ChatColor.WHITE);
+                                    break;
+                            }
+                    }
+            }
+        }
 }
