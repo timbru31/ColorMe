ef8b70defbc6df46f03be70bcb1f3c9117cdd82a
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2011-12-02 17:25:25 +0100

    made a new class for commands

--- src/de/xghostkillerx/colorme/ColorMe.java
+++ src/de/xghostkillerx/colorme/ColorMe.java
@@ -11,9 +11,7 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
 //import org.bukkit.World;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.craftbukkit.TextWrapper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Type;
@@ -21,6 +19,7 @@ import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 /**
  * ColorMe for CraftBukkit/Bukkit
@@ -43,8 +42,8 @@ public class ColorMe extends JavaPlugin {
 	public String pName;
 	public String df;
 	public String uf;
-	private Property conf;
-	public static Economy economy = null;
+	Property conf;
+	public Economy economy = null;
 	private void mkDir(String...d) {
 		for (String f : d) new File(f).mkdir();
 	}
@@ -59,11 +58,11 @@ public class ColorMe extends JavaPlugin {
         return false;
     }*/
 
-	private boolean self(Player p, String n) {
+	public boolean self(Player p, String n) {
 		return (p.equals(getServer().getPlayer(n))) ? true : false;
 	}
 
-	private void list(Player p) {
+	public void list(Player p) {
 		p.sendMessage("Color List:");
 		String color;
 		String msg = "";
@@ -157,187 +156,11 @@ public class ColorMe extends JavaPlugin {
 			 */
 			log.warning(String.format("[%s] Vault was NOT found! Running without economy!", getDescription().getName()));
 		}
-
-		// /color <color/name> [name] (name is optional since you can color your own name)
-		getCommand("colorme").setExecutor(new CommandExecutor() {
-			public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
-				if (sender instanceof Player && args.length>=1) {
-					Player player = ((Player)sender);
-					String a0 = args[0];
-					switch(args.length) {
-					case 1:
-						/* TODO: Add when possible to unregister events
-                                if (a0.equalsIgnoreCase("reload") && player.hasPermission("colorme.reload")) {
-                                    reload();
-                                    return true;
-                                }
-						 */
-						if (a0.equalsIgnoreCase("list") && player.hasPermission("colorme.list")) {
-							list(player);
-							return true;
-						}
-						if (hasColor(a0) && (player.hasPermission("colorme.remove") || self(player, a0))) {
-							removeColor(a0);
-							if (getServer().getPlayer(a0) != null) {
-								// Update displayname
-								Player other = getServer().getPlayer(a0);
-								other.setDisplayName(ChatColor.stripColor(other.getDisplayName()));
-								other.sendMessage(ChatColor.GREEN+"Your name color has been removed.");
-								if (other != sender) sender.sendMessage(ChatColor.GREEN+"Removed "+other.getName()+"'s color.");
-								return true;
-							}
-							sender.sendMessage((self(player, a0)) ? ChatColor.GREEN+"Removed your color." : ChatColor.GREEN+"Removed color from"+a0+'.');
-							return true;
-						}
-						if (!hasColor(a0) && colors.keyExists(a0)) return true; // Trying to remove a color from a color-less player
-						if (player.hasPermission("colorme.self")) {
-							player.sendMessage("STAGE1");
-							String color = findColor(a0);
-							if (color.equals(a0)) {
-								player.sendMessage(ChatColor.GREEN+"'"+a0+"' is not a supported color.");
-								return true;
-							}
-							if (economy != null) {
-								player.sendMessage("STAGE2");
-								double cost = conf.getDouble("cost");
-								if (cost>0 && economy.has(player.getName(), cost)) {
-									economy.withdrawPlayer(player.getName(), cost);
-									setColor(player.getName(), a0);
-									player.sendMessage(ChatColor.GREEN+"You have been charged "+ChatColor.RED+economy.format(cost)+ChatColor.GREEN+'.');
-									player.setDisplayName(ChatColor.valueOf(color)+ChatColor.stripColor(player.getDisplayName())+ChatColor.WHITE);
-								} else if (cost>0 && economy.getBalance(player.getName()) < cost) {
-									player.sendMessage(ChatColor.GREEN+"It costs "+ChatColor.GREEN+economy.format(cost)+ChatColor.GREEN+" to color your name.");
-								}
-							} else {
-								setColor(player.getName(), a0);
-								player.sendMessage("STAGE FAILED");
-								player.setDisplayName(ChatColor.valueOf(color)+ChatColor.stripColor(player.getDisplayName())+ChatColor.WHITE);
-								player.sendMessage(ChatColor.GREEN+"Your name color is now: "+ChatColor.valueOf(color)+a0);
-							}
-							return true;
-						} else if (!player.hasPermission("colorme.self")) {
-							player.sendMessage(ChatColor.GREEN+"You don't have permission to color your own name.");
-							return true;
-						}
-						break;
-					case 2:
-						String a1 = args[1];
-						String color = findColor(a1);
-						if (color.equals(a1)) {
-							player.sendMessage(ChatColor.GREEN+"'"+a1+"' is not a supported color.");
-							return true;
-						}
-						if (self(player, a0) && player.hasPermission("colorme.self")) {
-							// Coloring self
-							if (economy != null) {
-								// iConomy enabled
-								double cost = conf.getDouble("cost");
-								if (cost>0 && economy.getBalance(player.getName()) >= cost) {
-									// Player can afford to color their name
-									economy.withdrawPlayer(player.getName(), cost);
-									setColor(player.getName(), a1);
-									player.sendMessage(ChatColor.GREEN+"You have been charged "+ChatColor.RED+economy.format(cost)+ChatColor.GREEN+'.');
-									player.setDisplayName(ChatColor.valueOf(color)+ChatColor.stripColor(player.getDisplayName())+ChatColor.WHITE);
-								} else if (cost>0 && economy.getBalance(player.getName()) < cost) {
-									// Player can't afford to color their name
-									player.sendMessage(ChatColor.GREEN+"It costs "+ChatColor.RED+economy.format(cost)+ChatColor.GREEN+" to color your name.");
-								} else if (0 == cost) {
-									// No cost, color own name
-									setColor(player.getName(), a1);
-									player.setDisplayName(ChatColor.valueOf(color)+ChatColor.stripColor(player.getDisplayName())+ChatColor.WHITE);
-									player.sendMessage(ChatColor.GREEN+"Changed your name's color to: "+ChatColor.valueOf(color)+a1);
-								}
-								return true;
-							} else {
-								// iConomy NOT enabled
-								setColor(player.getName(), a1);
-								player.setDisplayName(ChatColor.valueOf(color)+ChatColor.stripColor(player.getDisplayName())+ChatColor.WHITE);
-								player.sendMessage(ChatColor.GREEN+"Changed your name's color to: "+ChatColor.valueOf(color)+a1);
-								return true;
-							}
-						} else if (!self(player, a0) && player.hasPermission("colorme.other")) {
-							// Coloring someone else
-							setColor(a0, a1);
-							if (getServer().getPlayer(a0) != null) {
-								Player other = getServer().getPlayer(a0);
-								other.setDisplayName(ChatColor.valueOf(color)+ChatColor.stripColor(player.getDisplayName())+ChatColor.WHITE);
-								player.sendMessage(ChatColor.GREEN+"Changed "+other.getName()+"'s color to: "+ChatColor.valueOf(color)+a1);
-								return true;
-							}
-							player.sendMessage(ChatColor.GREEN+"Changed "+a0+"'s color to: "+ChatColor.valueOf(color)+a1);
-							return true;
-						} else {
-							player.sendMessage(ChatColor.GREEN+"You don't have permission to color "+(self(player, a0) ? "your own" : "another player's")+" name.");
-							return true;
-						}
-					default: return false;
-					}
-				} else if (sender instanceof ConsoleCommandSender && args.length>=1) {
-					String a0 = args[0];
-					switch (args.length) {
-					case 1:
-						/* TODO: Add when possible
-                            if (a0.equalsIgnoreCase("reload")) {
-                                reload();
-                                return true;
-                            }
-						 */
-						if (a0.equalsIgnoreCase("list")) {
-							sender.sendMessage("Color List:");
-							String color;
-							String msg = "";
-							for (int i = 0; i < ChatColor.values().length; i++) {
-								color = ChatColor.getByCode(i).name();
-								if (msg.length() == 0) {
-									msg = ChatColor.valueOf(color)+color.toLowerCase().replace("_", "")+' ';
-									continue;
-								}
-								msg += (i == ChatColor.values().length-1) ? ChatColor.valueOf(color)+color.toLowerCase().replace("_", "") : ChatColor.valueOf(color)+color.toLowerCase().replace("_", "")+' ';
-								TextWrapper.wrapText(msg);
-							}
-							sender.sendMessage(msg);
-							return true;
-						}
-						if (hasColor(a0)) {
-							// Player has color, remove it
-							removeColor(a0);
-							if (getServer().getPlayer(a0) != null) {
-								// Player is online, update displayname
-								Player other = getServer().getPlayer(a0);
-								other.setDisplayName(ChatColor.stripColor(other.getDisplayName()));
-								other.sendMessage("Your name color has been removed.");
-								sender.sendMessage("Removed "+other.getName()+"'s color.");
-								return true;
-							}
-							sender.sendMessage("Removed color from "+a0);
-							return true;
-						}
-						sender.sendMessage(a0+" doesn't have a colored name.");
-						return true;
-					case 2:
-						String a1 = args[1];
-						if (setColor(a0, a1)) {
-							String color = findColor(a1);
-							if (getServer().getPlayer(a0) != null) {
-								// Player is online, change their displayname immediately
-								Player other = getServer().getPlayer(a0);
-								other.setDisplayName(ChatColor.valueOf(color)+ChatColor.stripColor(other.getDisplayName())+ChatColor.WHITE);
-								other.sendMessage("Your name color has been changed to "+a1);
-								sender.sendMessage("Changed "+other.getName()+"'s color to: "+a1);
-								return true;
-							}
-							sender.sendMessage("Changed "+a0+"'s color to: "+a1);
-							return true;
-						} else {
-							sender.sendMessage("'"+a1+"' is not a supported color.");
-							return false;
-						}
-					default: return false;
-					}
-				}
-				return false;
-			}
-		});
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+		ColorMeCommands cmd = new ColorMeCommands(this);
+		return cmd.ColorMeCommand(sender, command, commandLabel, args);
 	}
 
 	// Return the player's name color
