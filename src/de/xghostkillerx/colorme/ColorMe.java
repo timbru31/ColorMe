30a277ea644394c1d9284be9613a2e3c92f73e98
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2011-12-03 01:06:05 +0100

    rewrote the config stuff next are command (from scratch!)

--- src/de/xghostkillerx/colorme/ColorMe.java
+++ src/de/xghostkillerx/colorme/ColorMe.java
@@ -1,24 +1,28 @@
 package de.xghostkillerx.colorme;
 
 import java.io.File;
-import java.util.LinkedHashMap;
-//import java.util.Map;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.logging.Logger;
-
-import net.milkbowl.vault.Vault;
-import net.milkbowl.vault.economy.Economy;
-
 import org.bukkit.ChatColor;
-//import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.craftbukkit.TextWrapper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Type;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+//Economy (Vault)
+import net.milkbowl.vault.Vault;
+import net.milkbowl.vault.economy.Economy;
 
 
 /**
@@ -36,28 +40,100 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 
 public class ColorMe extends JavaPlugin {
-	public Logger log;
-	//public Map<World, Property> colors;
-	public Property colors;
-	public String pName;
-	public String df;
-	public String uf;
-	Property conf;
+	public static final Logger log = Logger.getLogger("Minecraft");
+
 	public Economy economy = null;
-	private void mkDir(String...d) {
-		for (String f : d) new File(f).mkdir();
+	public FileConfiguration config;
+	public FileConfiguration colors;
+	public File configFile;
+	public File colorsFile;
+
+	// Shutdown
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		log.info(pdfFile.getName() + " " + pdfFile.getVersion()	+ " has been disabled!");
 	}
-	/* TODO: Implement when events can be unregistered.
-    private void reload() {
-        getServer().getPluginManager().disablePlugin(this);
-        getServer().getPluginManager().enablePlugin(this);
-    }
 
-    private boolean isAdmin(Player p) {
-        if (p.getName().equalsIgnoreCase(conf.getString("admin"))) return true;
-        return false;
-    }*/
+	// Start
+	public void onEnable() {
+		
+		
 
+		colorsFile = new File(getDataFolder(), "players_color.yml");
+		if (!colorsFile.exists()) {
+			colorsFile.getParentFile().mkdirs();
+			copy(getResource("players_color.yml"), colorsFile);
+		}
+		colors = YamlConfiguration.loadConfiguration(colorsFile);
+
+		// Config
+		configFile = new File(getDataFolder(), "config.yml");
+		if (!configFile.exists()) {
+			configFile.getParentFile().mkdirs();
+			copy(getResource("config.yml"), configFile);
+		}
+		config = this.getConfig();
+		loadConfig();
+
+		// Events
+		//TODO Check if there are more event like chatting?!
+		getServer().getPluginManager().registerEvent(Type.PLAYER_JOIN, new ColorPlayerListener(this), Priority.Normal, this);
+
+		PluginDescriptionFile pdfFile = this.getDescription();
+		log.info(pdfFile.getName() + " " + pdfFile.getVersion() + " is enabled!");
+		Plugin x = this.getServer().getPluginManager().getPlugin("Vault");
+		if (x != null & x instanceof Vault) {
+			log.info(String.format(pdfFile.getName() + " loaded Vault successfully"));
+			setupEconomy();
+		} else {
+			log.warning(String.format("Vault was NOT found! Running without economy!"));
+		}
+	}
+
+	public void loadConfig() {
+		config.options().header("For help please refer to http://bit.ly/cookmebukkitdev or http://bit.ly/cookmebukkit");
+		config.addDefault("costs", 0);
+		config.options().copyDefaults(true);
+		saveConfig();
+	}
+
+	// Reloads the config via command /cookme reload
+	public void loadConfigAgain() {
+		try {
+			config.load(configFile);
+			saveConfig();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// If no config is found, copy the default one!
+	private void copy(InputStream in, File file) {
+		try {
+			OutputStream out = new FileOutputStream(file);
+			byte[] buf = new byte[1024];
+			int len;
+			while((len=in.read(buf))>0){
+				out.write(buf,0,len);
+			}
+			out.close();
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Initialized to work with Vault
+	private Boolean setupEconomy()
+	{
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+		if (economyProvider != null) {
+			economy = economyProvider.getProvider();
+		}
+
+		return (economy != null);
+	}
+	
 	public boolean self(Player p, String n) {
 		return (p.equals(getServer().getPlayer(n))) ? true : false;
 	}
@@ -78,102 +154,22 @@ public class ColorMe extends JavaPlugin {
 		p.sendMessage(msg);
 	}
 
-	private Boolean setupEconomy()
-	{
-		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-		if (economyProvider != null) {
-			economy = economyProvider.getProvider();
-		}
-
-		return (economy != null);
-	}
-
-	@Override
-	public void onDisable() {
-		// Clean up the garbage before disable
-		colors.clear();
-		colors = null;
-		conf.clear();
-		conf = null;
-		log = null;
-		pName = null;
-		df = null;
-		uf = null;
-
-		getServer().getLogger().info("[ColorMe] has been disabled.");
-	}
-
-	@Override
-	public void onEnable() {
-		log = getServer().getLogger();
-		pName = getDescription().getName();
-
-		df = getDataFolder().toString();
-		uf = df+"/../"+getServer().getUpdateFolder();
-		mkDir(df, uf);
-
-
-		/* Load each world's color list
-        for (World w : getServer().getWorlds()) {
-            Property worldFile = new Property(df+'/'+w.getName()+".color", this);
-            colors.put(w, worldFile);
-        }
-		 */
-		colors = new Property(df+'/'+"players.color", this);
-		colors.save();
-
-		// Does the config exist, if not then make a new blank one
-		if (!(new File(df+"/config.txt").exists())) {
-			conf = new Property(df+"/config.txt", this);
-			conf.setString("admin", "");
-			conf.setNumber("cost", 0);
-			conf.save();
-		} else {
-			conf = new Property(df+"/config.txt", this);
-			// Check if they have the updated prefix property file, otherwise update it to new format
-			if (!getDescription().getVersion().equalsIgnoreCase(conf.getString(pName+"Version"))) {
-				LinkedHashMap<String, Object> tmp = new LinkedHashMap<String, Object>();
-				conf.remove(pName+"Version");
-				tmp.put("admin", conf.getString("admin"));
-				for (String key : conf.getKeys())
-					tmp.put(key, conf.getString(key));
-				conf.rebuild(tmp);
-			}
-		}
-		getServer().getPluginManager().registerEvent(Type.PLAYER_JOIN, new ColorPlayerListener(this), Priority.Lowest, this);
-
-		log.info('['+pName+"] v"+getDescription().getVersion()+" has been enabled.");
-		Plugin x = this.getServer().getPluginManager().getPlugin("Vault");
-		if(x != null & x instanceof Vault) {
-			log.info(String.format("[%s] Enabled Version %s", getDescription().getName(), getDescription().getVersion()));
-			setupEconomy();
-		} else {
-			/**
-			 * Throw error & disable because we have Vault set as a dependency, you could give a download link
-			 * or even download it for the user.  This is all up to you as a developer to decide the best option
-			 * for your users!  For our example, we assume that our audience (developers) can find the Vault
-			 * plugin and properly install it.  It's usually a bad idea however.
-			 */
-			log.warning(String.format("[%s] Vault was NOT found! Running without economy!", getDescription().getName()));
-		}
-	}
-	
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
 		ColorMeCommands cmd = new ColorMeCommands(this);
-		return cmd.ColorMeCommand(sender, command, commandLabel, args);
+			return cmd.ColorMeCommand(sender, command, commandLabel, args);
 	}
 
 	// Return the player's name color
 	public String getColor(String name) {
-		return (colors.keyExists(name.toLowerCase())) ? colors.getString(name.toLowerCase()) : "";
+		return (colors.contains(name.toLowerCase())) ? colors.getString(name.toLowerCase()) : "";
 	}
 
 	// Set player's color and update displayname if online
 	public boolean setColor(String name, String color) {
 		String newColor = findColor(color); 
 		if (newColor.equals(color)) return false;
-		colors.setString(name.toLowerCase(), newColor);
-		colors.save();
+		colors.set(name.toLowerCase(), newColor);
+		saveColors();
 		if (getServer().getPlayer(name) != null) {
 			Player p = getServer().getPlayer(name);
 			p.setDisplayName(ChatColor.valueOf(newColor)+ChatColor.stripColor(p.getDisplayName())+ChatColor.WHITE);
@@ -181,7 +177,18 @@ public class ColorMe extends JavaPlugin {
 		return true;
 	}
 
+	private void saveColors() {
+		try {
+			colors.save(colorsFile);
+		} catch (IOException e) {
+			// TODO Ausfüllen
+			e.printStackTrace();
+		}
+		
+	}
+
 	// Iterate through colors to try and find a match (resource expensive)
+	// TODO verstehen!
 	public String findColor(String color) {
 		String col;
 		for (int i = 0; i <= 15; i++) {
@@ -193,15 +200,18 @@ public class ColorMe extends JavaPlugin {
 
 	// Check if a player has a color or not
 	public boolean hasColor(String name) {
-		return (colors.getString(name.toLowerCase()).trim().length()>1) ? true : false;
+		if (colors.contains(name.toLowerCase())) {
+			return (colors.getString(name.toLowerCase()).trim().length()>1) ? true : false;
+		}
+		return false;
 	}
 
 	// Removes a color if exists, otherwise returns false
 	public boolean removeColor(String name) {
 		name = name.toLowerCase();
 		if (hasColor(name)) {
-			colors.setString(name, "");
-			colors.save();
+			colors.set(name, "");
+			saveColors();
 			return true;
 		}
 		return false;
